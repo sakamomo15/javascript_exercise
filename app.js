ff5815829d092f995c68de48970b623d14c32cd9
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-03-04 13:49:35 +0000

    [Add] if

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('Hello World');
+let hello = "Hello, World"; //let 変数名 内容;
+alert(hello);
+
+// let int1 = 1;
+// alert(int1);
+
+// alert(4 + 3);
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
